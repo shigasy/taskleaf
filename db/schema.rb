b3edfb744c18b2806f9cbfb46863589e4d8403f0
MyPoZi <kxx555u.u@gmail.com>
2019-07-08 19:45:08 +0900

    [add] データベース上で外部キーを設定 UserとTask、Taskにuser_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_08_085407) do
+ActiveRecord::Schema.define(version: 2019_07_08_103833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_07_08_085407) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "{:null=>false, :index=>true}_id"
+    t.index ["{:null=>false, :index=>true}_id"], name: "index_tasks_on_{:null=>false, :index=>true}_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -28,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_07_08_085407) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
