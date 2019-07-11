7f1dd9a8aab052fc3c82cb13fb0d8f73190682cb
MyPoZi <kxx555u.u@gmail.com>
2019-07-11 17:24:14 +0900

    [add] 詳細表示のテスト追加

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 require 'factory_bot'
 
 describe 'タスク管理機能', type: :system do
+  let(:user_a) {FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')}
+  let(:user_b) {FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')}
+  let!(:task_a) {FactoryBot.create(:task, name: '最初のタスク', user: user_a)}
+
+  before do
+    visit login_path
+    fill_in 'メールアドレス', with: login_user.email
+    fill_in 'パスワード', with: login_user.password
+    click_button 'ログインする'
+  end
+
   describe '一覧表示機能' do
-    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
-    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
-
-    before do
-      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
-      visit login_path
-      fill_in 'メールアドレス', with: login_user.email
-      fill_in 'パスワード', with: login_user.password
-      click_button 'ログインする'
-    end
-
     context 'ユーザーAがログインしている時' do
       # ユーザーAでログインする
-      let(:login_user) { user_a }
+      let(:login_user) {user_a}
 
       it 'ユーザーAが作成した作成したタスクが表示される' do
         # 作成済みのタスクの名称が画面上に表示されていることを確認
@@ -26,10 +26,24 @@ describe 'タスク管理機能', type: :system do
 
     context 'ユーザーBがログインしている時' do
       # ユーザーBでログインする
-      let(:login_user) { user_b }
+      let(:login_user) {user_b}
 
       it 'ユーザーAが作成したタスクが表示されない' do
         expect(page).to have_no_content '最初のタスク'
+      end
+    end
+  end
+
+  describe '詳細表示機能' do
+    context 'ユーザーAがログインしているとき' do
+      let(:login_user) {user_a}
+
+      before do
+        visit task_path(task_a)
+      end
+
+      it 'ユーザーAが作成したタスクが表示される' do
+        expect(page).to have_content '最初のタスク'
       end
     end
   end
