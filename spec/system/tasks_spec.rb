278c134f59096565ab34ee05bd18eb128b125ca3
MyPoZi <kxx555u.u@gmail.com>
2019-07-11 16:18:40 +0900

    [add] タスク一覧表示機能のSystem Specを追加

---
+++ spec/system/tasks_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+require 'factory_bot'
+
+describe 'タスク管理機能', type: :system do
+  describe '一覧表示機能' do
+    before do
+      # ユーザーAを作成しておく
+      # user_a = FactoryBot.create(:user)
+      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
+      # 作成者がユーザーAであるタスクを作成しておく
+      FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+    end
+
+    context 'ユーザーAがログインしている時' do
+      before do
+        # ユーザーAでログインする
+        visit login_path
+        fill_in 'メールアドレス', with: 'a@example.com'
+        fill_in 'パスワード', with: 'password'
+        click_button 'ログインする'
+      end
+
+      it 'ユーザーAが作成した作成したタスクが表示される' do
+        # 作成済みのタスクの名称が画面上に表示されていることを確認
+        expect(page).to have_content '最初のタスク'
+      end
+    end
+  end
+end
