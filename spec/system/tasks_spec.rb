040de1cbfb5095d4a8b30be175ad7bf97af02702
MyPoZi <kxx555u.u@gmail.com>
2019-07-11 17:13:06 +0900

    [refactoring] let使って共通化

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,43 +3,33 @@ require 'factory_bot'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
+    let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
+    let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
+
     before do
-      # ユーザーAを作成しておく
-      # user_a = FactoryBot.create(:user)
-      user_a = FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com')
-      # 作成者がユーザーAであるタスクを作成しておく
       FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
 
     context 'ユーザーAがログインしている時' do
-      before do
-        # ユーザーAでログインする
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      # ユーザーAでログインする
+      let(:login_user) { user_a }
 
       it 'ユーザーAが作成した作成したタスクが表示される' do
         # 作成済みのタスクの名称が画面上に表示されていることを確認
         expect(page).to have_content '最初のタスク'
       end
+    end
 
     context 'ユーザーBがログインしている時' do
-      before do
-        # ユーザーBを作成しておく
-        FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com')
-        # ユーザーBでログインする
-        visit login_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      # ユーザーBでログインする
+      let(:login_user) { user_b }
 
       it 'ユーザーAが作成したタスクが表示されない' do
         expect(page).to have_no_content '最初のタスク'
-      end
-
       end
     end
   end
